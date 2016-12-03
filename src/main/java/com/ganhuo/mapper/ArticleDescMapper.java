c1667c6a3fec7d7e90034cfd0a4dc5e12191b2e0
sunzhiqiang <sunzhiqiang@hys-inc.cn>
2016-12-03 20:58:31 +0800

    改版后第一版，删除多余文件，适配360浏览器

---
+++ src/main/java/com/ganhuo/mapper/ArticleDescMapper.java
@@ -0,0 +1,71 @@
+package com.ganhuo.mapper;
+
+import com.ganhuo.model.domain.ArticleDesc;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+/**
+ * Created by sunzhiqiang on 2016/8/26.
+ */
+public interface ArticleDescMapper {
+
+    void insert(ArticleDesc ArticleDesc);
+
+    ArticleDesc getArticleById(Long id);
+
+    ArticleDesc getFirstArticle();
+
+    List<ArticleDesc> getArticleListByType(Integer typeId);
+
+    List<ArticleDesc> getRecentArticles();
+
+    void updateArticle(ArticleDesc ArticleDesc);
+
+    List<ArticleDesc> getMostHotArticle(@Param("i") int i);
+
+    List<ArticleDesc> getArticleByModule(List<Integer> param);
+
+    List<ArticleDesc> getAllArticle();
+
+    void updateCommentNum(@Param("id") Integer id);
+
+    ArticleDesc getLastArticle();
+
+    List<ArticleDesc> getArticleGroup();
+
+    List<ArticleDesc> getIndexArticle();
+
+    /**
+     * 获取最新的 几篇文章
+     * @param count
+     * @return
+     */
+    List<ArticleDesc> getLastArticles(@Param("count")Integer count);
+
+    /**
+     * 保存爬虫文章
+     * @param articleDescs
+     */
+    void batchInsertSpider(List<ArticleDesc> articleDescs);
+
+
+    /**
+     * 获取爬虫保存的 链接
+     * @return
+     */
+    List<ArticleDesc> getSpiderUrl();
+
+    /**
+     * 获取下一篇文章
+     * @param
+     * @return
+     */
+    ArticleDesc getNextArticleById(@Param("id")Long id);
+
+    /**
+     * 获取友情连接
+     * @return
+     */
+    List<ArticleDesc> getLinks();
+}
